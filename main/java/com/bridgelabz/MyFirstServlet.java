d838e0b6243063c056193cf32bacc5cf672d69fe
vishangi <vishangid@gmail.com>
2022-08-31 16:38:49 +0530

    Added : Welcome to my first servlet

---
+++ main/java/com/bridgelabz/MyFirstServlet.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/Hello")
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World Vishangi Trying again!</h3");
+        out.close();
+    }
+
+}
+
