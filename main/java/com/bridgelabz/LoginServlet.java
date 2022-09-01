ae62e8b49ec9b74f88719e2edd1114293526eceb
vishangi <vishangid@gmail.com>
2022-09-01 19:39:53 +0530

    Added : Extend the Servlet to accept a valid password

--- main/java/com/bridgelabz/LoginServlet.java
+++ main/java/com/bridgelabz/LoginServlet.java
@@ -31,7 +31,9 @@ public class LoginServlet extends HttpServlet {
         String userId = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
 
-        if (Pattern.matches("[A-Z][a-z]{2,}", userId) && (userId.equals(user) && password.equals(pwd))) {
+        if (Pattern.matches("[A-Z][a-z]{2,}", userId) &&
+                Pattern.matches("(?=.*?[A-Z])(?=.*?\\d)(?=.*?[a-z])(?=.*?[!@#$%^&*_()+-])[A-Za-z\\d!@#$%^&()*+_-]{8,}", password) &&
+                (userId.equals(user) && password.equals(pwd))) {
             request.setAttribute("user", user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
